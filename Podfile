d66296d9914c99760b48d0535b0a73f0debc0c75
lorne <280077576@qq.com>
2019-03-13 16:12:16 +0800

    second commit

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform:ios,'9.0'
+target 'TemPlateAPP' do
+pod 'Masonry'
+pod 'AFNetworking', '~> 3.0.0'
+pod 'SDWebImage', '~> 3.7.2'
+pod 'MBProgressHUD'
+pod 'MJRefresh'
+pod 'IQKeyboardManager'
+pod 'YYModel'
+end
---
+++ TemPlateAPP/Main/Common/BaseVC/BaseViewController.m
@@ -0,0 +1,32 @@
+//
+//  BaseViewController.m
+//  TemPlateAPP
+//
+//  Created by lorne on 2019/3/13.
+//  Copyright © 2019 ice. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
