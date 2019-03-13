a7bd3c3b32577a92291256a381343c61b40cc4ae
lorne <280077576@qq.com>
2019-03-13 16:18:37 +0800

    commit again

---
+++ TemPlateAPP/Main/Common/Classes/CustomerView.m
@@ -0,0 +1,21 @@
+//
+//  CustomerView.m
+//  TemPlateAPP
+//
+//  Created by lorne on 2019/3/13.
+//  Copyright © 2019 ice. All rights reserved.
+//
+
+#import "CustomerView.h"
+
+@implementation CustomerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ TemPlateAPP/Main/Home/Controller/HomeViewController.h
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.h
+//  TemPlateAPP
+//
+//  Created by lorne on 2019/3/13.
+//  Copyright © 2019 ice. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : BaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
